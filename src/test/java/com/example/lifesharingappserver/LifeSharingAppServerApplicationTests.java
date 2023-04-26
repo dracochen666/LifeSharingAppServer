c9b1f0f4e46dca5ddf6bbf46419d4b932fcb1a8c
chenlong <2474184221@qq.com>
2023-04-26 18:17:35 +0800

    first commit

---
+++ src/test/java/com/example/lifesharingappserver/LifeSharingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifesharingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSharingAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
